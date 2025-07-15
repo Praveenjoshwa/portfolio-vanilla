1ca9843575c574884e7756a05598daf4cb8375c3
Praveenjoshwa <bpj4510@gmail.com>
2025-07-15 12:16:26 +0530

    almost

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function toggleMenu(){
     icon.classList.toggle('open');
 
 }
+var typed = new Typed("#typed-role", {
+  strings: ["FRONTEND DEVELOPER", "FULLSTACK DEVELOPER", "WEB DEVELOPER", "BACKEND DEVELOPER"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  backDelay: 1000,
+  loop: true
+});
+
+
+
+
